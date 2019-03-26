cea632daccb62d8c984689204d3b6ee1f7fc1963
mahmoudezat1995 <mahmoudezat111@gmail.com>
2019-03-26 16:45:29 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -2,15 +2,16 @@ library(data.table)
 
         setwd("C:/Users/mahmoud/Documents/R_data")
         
-        all_data <- data.table::fread(input = "household_power_consumption.txt" ,na.strings = "?")
+        data <- data.table::fread(input = "household_power_consumption.txt" ,na.strings = "?")
         
-        all_data$Date <-as.Date(all_data$Date ,format = "%d/%m/%Y")
+        data$Date <-as.Date(data$Date ,format = "%d/%m/%Y")
 
-        all_data <- all_data[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+        data <- data[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
         
         png("plot1.png", width=480, height=480)
         
-        hist(all_data$Global_active_power ,col = "red",
+        hist(data$Global_active_power ,col = "red",
+             
         main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
         
         dev.off()
